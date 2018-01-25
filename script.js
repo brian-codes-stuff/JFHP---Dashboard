6965677f465bd1cbeda865f8c9c9206bdfd0dce6
brian-codes-stuff <connect@brianalonzo.com>
2018-01-25 14:04:27 -0500

    oops

--- script.js
+++ script.js
@@ -19,24 +19,5 @@
   var uid = null;
   var verifiedUser = false;
 
-
-
- // FirebaseUI config.
-      var uiConfig = {
-        signInSuccessUrl: 'dashboard.html',
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          
-          firebase.auth.EmailAuthProvider.PROVIDER_ID
-         
-        ],
-        // Terms of service url.
-        tosUrl: 'https://www.brianalonzo.com/terms'
-      };
-
-      // Initialize the FirebaseUI Widget using Firebase.
-       var ui = new  firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-    ui.start('#firebaseui-auth-container', uiConfig);
          
   
